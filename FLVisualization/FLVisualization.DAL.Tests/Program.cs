f5931dee05ac0b55131314cdfe93c2168e1f5f18
NikolaGrujic91 <grujic.nikola91@gmail.com>
2017-09-16 16:07:58 +0200

    Creation and configuring the solution and projects
    - Initial setup of project structure
    - Updated target frameworks
    - Added required packages

---
+++ FLVisualization/FLVisualization.DAL.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FLVisualization.DAL.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
