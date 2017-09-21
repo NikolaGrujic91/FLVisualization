6a409e1968b2671a8ded439755e5aadc80f8c5e5
NikolaGrujic91 <grujic.nikola91@gmail.com>
2017-09-21 11:15:11 +0200

    Configured services for startup

--- FLVisualization/FLVisualization.Service/Startup.cs
+++ FLVisualization/FLVisualization.Service/Startup.cs
@@ -7,6 +7,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using FLVisualization.DAL.EF;
+using FLVisualization.DAL.Repos;
+using FLVisualization.DAL.Repos.Interfaces;
+
 
 namespace FLVisualization.Service
 {
@@ -28,14 +35,40 @@ namespace FLVisualization.Service
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvcCore()
+                .AddJsonFormatters(j =>
+                {
+                    j.ContractResolver = new DefaultContractResolver(); // To Pascal Casing
+                    j.Formatting = Formatting.Indented; // Indented formatting
+                });
+
+            // Allow all http request types
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials();
+                });
+            });
+
+            // Configure EF Core
+            services.AddDbContext<FLVisualizationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("FLVisualization")));
+
+            // Configure the Dependency Injection Container
+            services.AddScoped<ITeamRepo, TeamRepo>();
+            services.AddScoped<IPositionRepo, PositionRepo>();
+            services.AddScoped<IPlayerRepo, PlayerRepo>();
+            services.AddScoped<IPlayerHistoryRepo, PlayerHistoryRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            if (env.IsDevelopment())
+            {
+                loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+                loggerFactory.AddDebug();
+            }
 
             app.UseMvc();
         }
